a5b6f5812a41d62e34c14d15c77bafe86ae1ec3d
Alessandro Mastrofini <alessandro.mastrofini1@gmail.com>
2025-03-11 17:40:07 +0100

    Updates with wrong code

--- main.cs
+++ main.cs
@@ -28,29 +28,29 @@ class Program
         
 
         // v(0) = 0
-        double[] row1 = {0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0 };
+        double[] row1 = { 0, 0, 0, 1/(E*I), 0, 0, 0, 0, 0, 0, 0, 0 };
         // w(0) = 0
-        double[] row2 = {0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0 };
+        double[] row2 = {0, 0, 0, 0, 0, -1/(E*A), 0, 0, 0, 0, 0, 0 };
         // M(0) = 0
         double[] row3 = { 0, -1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0 };
         // v(zF_s0) = 0
-        double[] row4 = {0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0 };
+        double[] row4 = { 1/(E*I)*Math.Pow(zF_s0,3)/6,  1/(E*I)*Math.Pow(zF_s0,2)/2, 1/(E*I)*zF_s0,  1/(E*I), 0, 0, 0, 0, 0, 0, 0, 0 };
         // Delta_w() = w_s1(0) - w_s0(zF_s0) = 0
-        double[] row5 = {0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0 };
+        double[] row5 = { 0, 0, 0, 0, -(-1/(E*A)) *zF_s0,  -(-1/(E*A)), 0, 0, 0, 0, 0, -1/(E*A) };
         // Delta_phi() = phi_s1(0) - phi_s0(zF_s0) = 0
         double[] row6 = { -(1/(E*I))*Math.Pow(zF_s0,2)/2, -(1/(E*I))*zF_s0, (1/(E*I)), 0, 0, 0, 0, 0,  -1/(E*I), 0, 0, 0 };
         // Delta_v() = v_s1(0) - v_s0(zF_s0) = 0
-        double[] row7 = {0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0 };
+        double[] row7 = {-(1/(E*I))*Math.Pow(zF_s0,3)/6, -(1/(E*I))*Math.Pow(zF_s0,2)/2, -(1/(E*I))*zF_s0,  -(1/(E*I)), 0, 0, 0, 0, 0,  1/(E*I), 0, 0 };
         // Delta_M() = M_s1(0) - M_s0(zF_s0) = 0
-        double[] row8 = {0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0 };
+        double[] row8 = { -(-zF_s0), -(-1), 0, 0, 0, 0, 0, -1, 0, 0, 0, 0 };
         // Delta_N() = N_s1(0) - N_s0(zF_s0) = 0
-        double[] row9 = {0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0 };
+        double[] row9 = { 0, 0, 0, 0, -(-1), 0, 0, 0, 0, 0, -1, 0 };
         // M(zF_s1) = 0
         double[] row10 = {0, 0, 0, 0, 0, 0, -zF_s1, -1, 0, 0, 0, 0 };
         // T(zF_s1) = F
         double[] row11 = {0, 0, 0, 0, 0, 0, -1, 0, 0, 0, 0, 0 };
         // N(zF_s1) = 0
-        double[] row12 = {0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0 };
+        double[] row12 = {0, 0, 0, 0, 0, 0, 0, 0, 0, 0, -1, 0 };
         // Set rows collection for the matrix
         List<double[]> rows = new List<double[]>
         {
@@ -60,7 +60,7 @@ class Program
         // define the vector
         double[] vector = new double[]
         {
-           0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0
+           0, 0, 0, 0, 0, 0, 0, 0, 0, 0, force, 0
         };
 
         
